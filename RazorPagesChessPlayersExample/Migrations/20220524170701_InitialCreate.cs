509b6bbee838b08454605bf7d25722f74a279f20
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-24 13:11:01 -0400

    Created the migration script for the database and updated the database with it

---
+++ RazorPagesChessPlayersExample/Migrations/20220524170701_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesChessPlayersExample.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChessPlayer",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FullName = table.Column<string>(type: "TEXT", nullable: true),
+                    DateOfTitleAcquisition = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CurrentTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    CurrentRating = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChessPlayer", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChessPlayer");
+        }
+    }
+}
