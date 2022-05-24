a06aff307c2b30abdec3030d327d55883a52e1be
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-24 13:05:53 -0400

    Updated the startup.cs with the steps outlined on the tutorial
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/model?view=aspnetcore-5.0&tabs=visual-studio-code#use-sqlite-for-development-sql-server-for-production

--- RazorPagesChessPlayersExample/Startup.cs
+++ RazorPagesChessPlayersExample/Startup.cs
@@ -14,20 +14,32 @@ namespace RazorPagesChessPlayersExample
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Environment.IsDevelopment())
+            {
+
+                services.AddDbContext<RazorPagesChessPlayersExampleContext>(options =>
+                        options.UseSqlite(Configuration.GetConnectionString("RazorPagesChessPlayersExampleContext")));
+            }
+            else
+            {
+                services.AddDbContext<RazorPagesChessPlayersExampleContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("RazorPagesChessPlayersExampleContext")));
+            }
             services.AddRazorPages();
 
-            services.AddDbContext<RazorPagesChessPlayersExampleContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesChessPlayersExampleContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
