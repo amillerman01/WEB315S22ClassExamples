17483436fc9a6693116ed3e600bd176355338c6d
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-24 12:00:51 -0400

    Added enum example, documentation to follow later

--- RazorPagesClassExample/Pages/Index.cshtml.cs
+++ RazorPagesClassExample/Pages/Index.cshtml.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RazorPagesClassExample.Models;
 
 namespace RazorPagesClassExample.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        enum Colors { 
+            Red = 1, 
+            Green = 2, 
+            Blue = 4, 
+            Yellow = 8 
+        };
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +26,41 @@ namespace RazorPagesClassExample.Pages
 
         public void OnGet()
         {
+            var someColourFromTheUser = 2; // Green
 
+            // if (someColourFromTheUser == 1) 
+            if (someColourFromTheUser == (int)Colors.Red) 
+            {
+                // do something specific when it's red
+            }
+            else if (someColourFromTheUser == (int)Colors.Green) 
+            {
+                // something specific when it's green
+            }
+            else if (someColourFromTheUser == 4)
+            {
+                // something specific when it's blue
+            }
+            else if (someColourFromTheUser == 8)
+            {
+                // something specific when it's yellow
+            }
+
+            // switch(someColourFromTheUser){
+            //     case (int)Colors.Red:
+            //     {
+
+            //     }
+            //     case (int)Colors.Green:{
+
+            //     }
+            // }
+        }
+/**
+* param(somevalue) - it does something
+*/
+        public void doAThing(string somevalue){
+            // will do something
         }
     }
 }
