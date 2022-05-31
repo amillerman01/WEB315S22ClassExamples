13fb3309ca224db92e79a783fdf0c6602b42b664
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-31 12:27:10 -0400

    Updated our model to contain display names for our fields

--- RazorPagesChessPlayersExample/Models/ChessPlayer.cs
+++ RazorPagesChessPlayersExample/Models/ChessPlayer.cs
@@ -13,11 +13,16 @@ namespace RazorPagesChessPlayersExample.Models
         //     return this.FirstName + " " + this.LastName;
         // } }
 
+        [Display(Name = "Name")]
         public string FullName { get; set; }
+        [Display(Name = "Date Title was Acquired")]
         [DataType(DataType.Date)]
         public DateTime DateOfTitleAcquisition { get; set; }
+        [Display(Name = "Player Title")]
         public string CurrentTitle { get; set; }
+        [Display(Name = "Rating")]
         public int CurrentRating { get; set; }
+        [Display(Name = "Tournament Victories")]
         public int NumberOfTournamentVictories { get; set; }
     }
 }
