0faced35e85939ada7ccc29d4655af340dedc5b4
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-07 11:48:57 -0400

    Added regex expressions with custom error messages

--- RazorPagesChessPlayersExample/Models/ChessPlayer.cs
+++ RazorPagesChessPlayersExample/Models/ChessPlayer.cs
@@ -15,6 +15,7 @@ namespace RazorPagesChessPlayersExample.Models
         // } }
 
         [Display(Name = "Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "{0} must start with a capital letter and only contain letters or spaces")]
         [Required]
         [StringLength(60, MinimumLength = 3)]
         public string FullName { get; set; }
@@ -24,6 +25,7 @@ namespace RazorPagesChessPlayersExample.Models
         [Required]
         public DateTime DateOfTitleAcquisition { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "{0} must start with a capital letter, and can contain letters, numbers, spaces, hyphens and quotes")]
         [Display(Name = "Player Title")]
         public string CurrentTitle { get; set; }
 
@@ -37,6 +39,7 @@ namespace RazorPagesChessPlayersExample.Models
         public int NumberOfTournamentVictories { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
         [Range(0.0, Double.MaxValue, ErrorMessage = "The value of {0} must be greater than {1}.")]
         [Required]
         public decimal AnnualWageAsChessPlayer { get; set; }
