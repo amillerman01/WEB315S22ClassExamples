cec3443e7fd10e38fef23ec717901e4e666d6bd3
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-07 12:34:21 -0400

    Updated the database based on our validation additions

---
+++ RazorPagesChessPlayersExample/Migrations/20220607162920_ChessPlayerValidation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesChessPlayersExample.Migrations
+{
+    public partial class ChessPlayerValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "ChessPlayer",
+                type: "TEXT",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "ChessPlayer",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 60);
+        }
+    }
+}
