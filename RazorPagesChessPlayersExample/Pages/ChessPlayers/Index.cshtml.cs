ad2cf3fe767362b591e88c0e9417e4c316bfa932
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-10 11:38:52 -0400

    Added functionality to filter by chess currentTitle

--- RazorPagesChessPlayersExample/Pages/ChessPlayers/Index.cshtml.cs
+++ RazorPagesChessPlayersExample/Pages/ChessPlayers/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesChessPlayersExample.Models;
 
@@ -26,22 +27,50 @@ namespace RazorPagesChessPlayersExample.Pages_ChessPlayers
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
 
+        public SelectList CurrentTitles { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ChessPlayerCurrentTitle { get; set; }
+
         // public async Task OnGetAsync()
         // {
         //     ChessPlayer = await _context.ChessPlayer.ToListAsync();
         //     // SomeValue = 10;
         // }
 
+        // public async Task OnGetAsync()
+        // {
+        //     var chessPlayers = from c in _context.ChessPlayer
+        //                         select c;
+
+        //     if (!string.IsNullOrEmpty(SearchString))
+        //     {
+        //         chessPlayers = chessPlayers.Where(s => s.FullName.Contains(SearchString));
+        //     }
+
+        //     ChessPlayer = await chessPlayers.ToListAsync();
+        // }
+
         public async Task OnGetAsync()
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> currentTitleQuery = from c in _context.ChessPlayer
+                                            orderby c.CurrentTitle
+                                            select c.CurrentTitle;
+
             var chessPlayers = from c in _context.ChessPlayer
-                                select c;
+                        select c;
 
             if (!string.IsNullOrEmpty(SearchString))
             {
                 chessPlayers = chessPlayers.Where(s => s.FullName.Contains(SearchString));
             }
 
+            if (!string.IsNullOrEmpty(ChessPlayerCurrentTitle))
+            {
+                chessPlayers = chessPlayers.Where(x => x.CurrentTitle == ChessPlayerCurrentTitle);
+            }
+            CurrentTitles = new SelectList(await currentTitleQuery.Distinct().ToListAsync());
             ChessPlayer = await chessPlayers.ToListAsync();
         }
     }
