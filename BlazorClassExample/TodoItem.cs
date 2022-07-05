21929832e242a095b4c5bca954e676ace056d5dc
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-07-05 11:33:40 -0400

    Added the empty list to the todo.razor file

--- BlazorClassExample/TodoItem.cs
+++ BlazorClassExample/TodoItem.cs
@@ -1,5 +1,8 @@
 public class TodoItem
 {
+    TodoItem(){
+        System.Console.WriteLine("Created a new instance of a TodoItem");
+    }
     public string Title { get; set; }
     public bool IsDone { get; set; }
 }
