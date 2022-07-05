d899f83246395f191654988e1a1c33a73e226bf2
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-07-05 12:18:07 -0400

    Added some complexity to the class example
    - Added css for our Todo page
    - Added a second constructor to the TodoItem, that takes a string input and sets the isDone to true by default
    - Moved the html around for the list being displayed, so that it's inside of a label

--- BlazorClassExample/TodoItem.cs
+++ BlazorClassExample/TodoItem.cs
@@ -1,8 +1,17 @@
 public class TodoItem
 {
+    // we can declare the constructor here it we want to do something in it
     public TodoItem(){
         System.Console.WriteLine("Created a new instance of a TodoItem");
     }
+
+    // a second constructor that can take a custom string value, and will set the isDone value to true by default
+    public TodoItem(string title){
+        Title = title;
+        IsDone = true;
+        System.Console.WriteLine("Created a new instance of a TodoItem, with a custom string");
+    }
+
     public string Title { get; set; }
     public bool IsDone { get; set; }
 }
