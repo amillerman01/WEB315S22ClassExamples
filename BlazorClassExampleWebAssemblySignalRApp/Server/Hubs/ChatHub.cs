56d9cdd2a3c70f672abadb9350387951f105f888
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-07-12 13:25:01 -0400

    Added more examples of sending and receiving messages

--- BlazorClassExampleWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
+++ BlazorClassExampleWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
@@ -9,5 +9,17 @@ namespace BlazorClassExampleWebAssemblySignalRApp.Server.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+        public async Task SendMessageToCaller(string user, string message)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task SendMessageToOthers(string user, string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessageOthers", user, message);
+        }
+        public async Task SendPingToOthers(string user)
+        {
+            await Clients.Others.SendAsync("PingUsers", user);
+        }
     }
 }
