5e910545a9cc9980749e9b86b7444325a6d74772
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-07-12 11:55:11 -0400

    Completed the server configuration
    Make sure to change your namespace on the chathub!

--- BlazorClassExampleWebAssemblySignalRApp/Server/Startup.cs
+++ BlazorClassExampleWebAssemblySignalRApp/Server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using BlazorClassExampleWebAssemblySignalRApp.Server.Hubs;
 
 namespace BlazorClassExampleWebAssemblySignalRApp.Server
 {
@@ -22,7 +23,12 @@ namespace BlazorClassExampleWebAssemblySignalRApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -30,6 +36,8 @@ namespace BlazorClassExampleWebAssemblySignalRApp.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -52,6 +60,7 @@ namespace BlazorClassExampleWebAssemblySignalRApp.Server
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
                 endpoints.MapFallbackToFile("index.html");
             });
         }
