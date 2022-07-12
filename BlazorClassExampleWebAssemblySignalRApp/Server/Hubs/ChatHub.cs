44bb8184a9a2fc2806182c28fa3f3b7e2c3ceb17
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-07-12 14:07:37 -0400

    Added the ability to send a string and number across signalR
    Bonus example

--- BlazorClassExampleWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
+++ BlazorClassExampleWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -21,5 +22,14 @@ namespace BlazorClassExampleWebAssemblySignalRApp.Server.Hubs
         {
             await Clients.Others.SendAsync("PingUsers", user);
         }
+        public async Task SendPingToCaller(string user)
+        {
+            var rand = new Random();
+            int numberFromServer = rand.Next(1000);
+            // the number of values sent back to the client doesn't always have to be the same number as the values it received
+            // We received 1 value (user), and sent 2 back (user and a random number)
+            await Clients.Caller.SendAsync("PingMyself", user, numberFromServer);
+        }
+
     }
 }
